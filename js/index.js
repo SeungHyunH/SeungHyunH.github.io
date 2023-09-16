ca564ece29efa758b3ed2fe6745d7f9deed1b810
한승현 <vkflek1232@naver.com>
2023-09-16 22:32:05 +0900

    header 애니메이션 추가 (nav 휴대폰화면 분기처리 필요)

--- js/index.js
+++ js/index.js
@@ -1,11 +1,30 @@
 const btnViewMenu = document.querySelector(".btn_open_menu");
 const gnb = document.querySelector(".gnb");
+
+const init = ()=> {
+  if( window.matchMedia('(min-width:769px)').matches ){
+    btnViewMenu.classList.add('hide');
+    gnb.classList.remove('hide');
+  }else{
+    btnViewMenu.classList.remove('hide');
+    gnb.classList.add('hide');
+  }
+}
+
+
 window.addEventListener('click',e =>{
   const openMenu = e.target.closest(".btn_open_menu");
-  if(openMenu && openMenu.classList[0] === "btn_open_menu"){
-    console.log("메뉴버튼클릭");
+  if(openMenu && openMenu.classList[0]==="btn_open_menu"){
+    if(openMenu.classList[1]==="close"){
+      openMenu.classList.remove("close");
+      openMenu.classList.add("open");
+    }else{
+      openMenu.classList.remove("open");
+      openMenu.classList.add("close");
+    }
   }
 });
+
 window.addEventListener('resize',e =>{
   if( window.matchMedia('(min-width:769px)').matches ){
     btnViewMenu.classList.add('hide');
@@ -15,3 +34,5 @@ window.addEventListener('resize',e =>{
     gnb.classList.add('hide');
   }
 });
+
+init();
